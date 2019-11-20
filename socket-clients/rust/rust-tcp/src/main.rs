cbe033b8528b976fa3311a403f1d9c10334fd0fa
yancy <yancy.ribbens@gmail.com>
2019-11-20 08:39:32 -0600

    send subscribe and read from socket

--- socket-clients/rust/rust-tcp/src/main.rs
+++ socket-clients/rust/rust-tcp/src/main.rs
@@ -7,17 +7,24 @@ fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("172.32.0.3:60401")?;
     stream.set_nonblocking(true).expect("set_nonblocking call failed");
 
-    let s = "{\"method\":\"server.banner\",\"params\":[],\"id\":90003}\r\n".to_string(); 
-    stream.write(&s.as_bytes())?;
+    let banner = "{\"method\":\"server.banner\",\"params\":[],\"id\":90003}\r\n".to_string(); 
+    let version = "{\"method\":\"server.version\",\"params\":[],\"id\":90003}\r\n".to_string();
+    let subscribe = "{\"method\":\"blockchain.headers.subscribe\",\"params\":[],\"id\":90003}\r\n".to_string();
+
+    stream.write(&subscribe.as_bytes())?;
 
     let mut line;
     loop {
 	line = [0; 512];
 	let result = stream.read(&mut line);
 	match result {
-	    Ok(n) => println!("Received {} bytes", str::from_utf8(&line).unwrap()),
-	    _ => {},
-	}
+	    Ok(n) => {
+                if n > 0 {
+                    println!("{}: Received: {}", n, str::from_utf8(&line).unwrap());
+                }
+            },
+	    _ => { },
+	};
     }
 
     Ok(())
