d836c5e26b9eac3240f26651fbef466e0b3ce696
yancy <yancy.ribbens@gmail.com>
2019-11-20 07:35:58 -0600

    print socket results

--- socket-clients/rust/rust-tcp/src/main.rs
+++ socket-clients/rust/rust-tcp/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
-use std::io::{self, Read};
+use std::io::{self, Read, BufReader};
 use std::net::TcpStream;
+use std::str;
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("172.32.0.3:60401")?;
@@ -9,6 +10,15 @@ fn main() -> std::io::Result<()> {
     let s = "{\"method\":\"server.banner\",\"params\":[],\"id\":90003}\r\n".to_string(); 
     stream.write(&s.as_bytes())?;
 
-    stream.read(&mut [0; 128])?;
+    let mut line;
+    loop {
+	line = [0; 512];
+	let result = stream.read(&mut line);
+	match result {
+	    Ok(n) => println!("Received {} bytes", str::from_utf8(&line).unwrap()),
+	    _ => {},
+	}
+    }
+
     Ok(())
 }
