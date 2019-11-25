809994e9c2e1666ef52f1af88d5a9a5f40be8397
yancy <yancy.ribbens@gmail.com>
2019-11-25 00:48:14 -0600

    implement async await

--- socket-clients/rust/rust-tokio/src/main.rs
+++ socket-clients/rust/rust-tokio/src/main.rs
@@ -1,43 +1,21 @@
-extern crate tokio;
-
-use tokio::io;
-use tokio::net::TcpStream;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWrite;
+use std::error::Error;
 
-pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "172.32.0.3:60401".parse()?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    let mut stream = TcpStream::connect("172.32.0.3:60401").await?;
+    let subscribe = "{\"method\":\"blockchain.headers.subscribe\",\"params\":[],\"id\":90003}\r\n".to_string();
+    stream.write_all(&subscribe.as_bytes()).await?;
 
-    // Open a TCP stream to the socket address.
-    //
-    // Note that this is the Tokio TcpStream, which is fully async.
-    let client = TcpStream::connect(&addr)
-        .and_then(|stream| {
-            println!("created stream");
-            io::write_all(stream, "{\"method\":\"server.banner\",\"params\":[],\"id\":90003}\r\n").then(|result| {
-                println!("wrote to stream; success={:?}", result.is_ok());
-                Ok(())
-            })
-        })
-        .map_err(|err| {
-            // All tasks must have an `Error` type of `()`. This forces error
-            // handling and helps avoid silencing failures.
-            //
-            // In our example, we are only going to log the error to STDOUT.
-            println!("connection error = {:?}", err);
-        });
-
-    // Start the Tokio runtime.
-    //
-    // The Tokio is a pre-configured "out of the box" runtime for building
-    // asynchronous applications. It includes both a reactor and a task
-    // scheduler. This means applications are multithreaded by default.
-    //
-    // This function blocks until the runtime reaches an idle state. Idle is
-    // defined as all spawned tasks have completed and all I/O resources (TCP
-    // sockets in our case) have been dropped.
-    println!("About to create the stream and write to it...");
-    tokio::run(client);
-    println!("Stream has been created and written to.");
+    loop {
+        let mut buf = [0; 1024];
+        let n = stream.read(&mut buf).await?;
+        let recieved = String::from_utf8(buf[0..n].to_vec())?;
+        println!("They sent: {:?}", recieved);
+    }
 
     Ok(())
 }
